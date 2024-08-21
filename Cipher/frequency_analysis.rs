db5bb2dbd5d35d7efb9a9e559ec78f6c98ce3489
fosterchild <110239648+fosterchild1@users.noreply.github.com>
2024-08-21 20:58:58 +0300

    frequency analysis supports numbers

--- Cipher/frequency_analysis.rs
+++ Cipher/frequency_analysis.rs
@@ -1,12 +1,18 @@
-const alphabet: [char; 26] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-/*const alphabet_iter: Iterator<Item = Vec> = alphabet.iter();*/
+const DICTIONARY: [char; 36] = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 pub fn main(string_: String) -> Vec<i8> {
-    let split: Vec<char> = string_.chars().collect();
-    let mut analysis: Vec<i8> = vec![0; 26];
-    for character in split {
-        let index = (character.to_ascii_lowercase() as usize) - ('a' as usize);
-        analysis[index] += 1
+    let mut analysis: Vec<i8> = vec![0; 36];
+    for character in string_.chars() {
+        let lowercase_char = character.to_lowercase().next().unwrap();
+        let index = if lowercase_char.is_ascii_alphabetic() {
+            (lowercase_char as usize) - ('a' as usize)
+        } else if lowercase_char.is_ascii_digit() {
+            26 + (lowercase_char as usize) - ('0' as usize)
+        } else {
+            continue;
+        };
+        analysis[index] += 1;
     }
-    return analysis
+    return analysis;
 }
