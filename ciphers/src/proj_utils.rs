615ccde80f1c8be8c12f6be953c917091718219d
fosterchild <110239648+fosterchild1@users.noreply.github.com>
2024-08-28 20:50:48 +0300

    Delete ciphers directory

--- ciphers/src/proj_utils.rs
+++
@@ -1,8 +0,0 @@
-pub fn get_from_analysis(analysis: Vec<(String, i32)>, idx: usize) -> String {
-    // this is ugly
-    let mut actual_index = idx.clone();
-    if idx >= analysis.len() {
-        actual_index = 0;
-    }
-    return (analysis[actual_index].0.parse::<i8>().unwrap() as u8 as char).to_string();
-}
